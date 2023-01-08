ab89145ac9662b49f83aa74b8979a1abda747279
Igor Olkhov <smartestbox@gmail.com>
2023-01-08 18:03:26 +0100

    Create Technologies and Header components

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import exp from "constants";
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    )
+}
+
+export default Header;
